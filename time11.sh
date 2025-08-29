e6b98f8b9110e23dbd502950e2dc9ebaea50c208
vpsmajjani <vpsmajjani@gmail.com>
2025-08-29 17:07:21 +0530

    adding updated files

--- time11.sh
+++ time11.sh
@@ -1,10 +1,11 @@
 current_hour=$(date +%H)
 
-if [ "$current_hour" -eq 11 -o "$current_hour" -eq 23 ]; then
+if [ "$current_hour" -eq 5 -o "$current_hour" -eq 17 ]; then
 echo "Taking backup...at 11"
 cd /home/manish/puppeteer-scraper/flipkart_urls
 sh backuptak1.sh
     # Your backup commands here
+echo "successfully backup taken in tar at `date`"
 else
 echo "exiting, not 11pm or am for backup time"
 fi
