994582533821e81623666c8cf0995e94171711f6
vpsmajjani <vpsmajjani@gmail.com>
2025-08-29 13:49:36 +0530

    commited after taking backup

--- time11.sh
+++ time11.sh
@@ -2,7 +2,9 @@ current_hour=$(date +%H)
 
 if [ "$current_hour" -eq 13 ]; then
     echo "Taking backup..."
+cd /home/manish/puppeteer-scraper/flipkart_urls
+sh backuptak1.sh
     # Your backup commands here
 else
-echo "exiting"
+echo "exiting, not 11pm or am"
 fi
