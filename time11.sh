3e471409aa4ea3c6631edb18b50c166bd957f149
vpsmajjani <vpsmajjani@gmail.com>
2025-08-29 13:55:58 +0530

    commited after taking backup

--- time11.sh
+++ time11.sh
@@ -6,5 +6,5 @@ cd /home/manish/puppeteer-scraper/flipkart_urls
 sh backuptak1.sh
     # Your backup commands here
 else
-echo "exiting, not 11pm or am"
+echo "exiting, not 11pm or am for backup time"
 fi
